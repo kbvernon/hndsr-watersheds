cb19c439e1c159a7055d1d2a79e8b9b32ea76235
kbvernon <kenneth.b.vernon@gmail.com>
2023-01-25 11:23:11 -0700

    observable.js 😖

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 
-This small repo is for sharing a Leaflet map through Github Pages, so PIs on an NSF HNDS-R grant can explore USGS HUC10 watersheds over parts of Arizona and New Mexico.
+This small repo is for sharing an [Observable](https://observablehq.com/) + [Leaflet](https://leafletjs.com/) app, built with much consternation 😖 and love ❤️ using [Quarto](https://quarto.org/). It's a simple app, probably a smidge over-engineered for its purpose, but it did give me the opportunity to do a deep dive into the Observable dialect of JS, for whatever that's worth. Anyway, may it serve the PIs on this NSF HNDS-R well as they explore the watersheds of Arizona and New Mexico. 🖖 LL&P
 
--- download-watersheds.R
+++ download-watersheds.R
@@ -1,128 +1,141 @@
-library(mapview)
+
+library(here)
+library(httr2)
+library(lwgeom)
 library(rmapshaper)
 library(sf)
 library(tidyverse)
-library(magrittr)
 
-simple_hydro_read <- function(x){
+# download watersheds -----------------------------------------------------
+
+download_hydro <- function(x, out_dir){
   
-  out <-
-    x %>%
-    sf::read_sf(layer = "WBDHU10") %>%
-    dplyr::select(HUC10, Name) %>%
-    dplyr::rename(`Hydrologic_Unit` = HUC10) %>%
-    magrittr::set_names(c("Hydrologic_Unit","Watershed","geometry")) %>%
-    sf::st_set_geometry("geometry")
+  out <- file.path(out_dir, paste0("NHDPLUS_H_", x, "_HU4_GDB.gdb"))
   
-  hydrologic_units <-
-    c("Subregion" = 4,
-      "Basin" = 6,
-      "Subbasin" = 8)
+  if(file.exists(out)) return(out)
   
-  for(i in seq(4,8,2)){
-    out %<>%
-      dplyr::mutate(HUC = stringr::str_sub(`Hydrologic_Unit`, end = i)) %>%
-      dplyr::left_join(
-        sf::read_sf(x,
-                                   layer = paste0("WBDHU",i)) %>%
-                         sf::st_drop_geometry() %>%
-                         dplyr::rename(HUC = paste0("HUC",i)) %>%
-                         dplyr::select(HUC, Name) %>%
-                         magrittr::set_names(c("HUC",
-                                               names(hydrologic_units)[hydrologic_units == i])) %>%
-                         dplyr::distinct()
-        ) %>%
-      dplyr::select(-HUC)
-  }
-  out %>%
-    dplyr::select(`Hydrologic_Unit`, Subregion:Subbasin, Watershed, geometry) %>%
-    sf::st_transform(4326)
+  base_url <- "https://prd-tnm.s3.amazonaws.com/StagedProducts/"
+  product <- "Hydrography/NHDPlusHR/Beta/GDB/"
+  gdb <- paste0("NHDPLUS_H_", x, "_HU4_GDB.zip")
+  
+  tmp <- tempfile(fileext = ".zip")
+  
+  download.file(paste0(base_url, product, gdb), tmp, mode = "wb")
+  
+  unzip(tmp, exdir = out_dir)
+  
+  return(out)
+  
 }
 
+read_hydro <- function(x){
+  
+  out <- read_sf(x, layer = "WBDHU10") |> 
+    rename(
+      "watershed" = Name,
+      "hydrologic_unit" = HUC10,
+      "geometry" = Shape 
+    ) |> 
+    st_set_geometry("geometry") |> 
+    select(hydrologic_unit, watershed)
+  
+  hydrologic_units <- c("subregion" = 4, "basin" = 6, "subbasin" = 8)
+  
+  for(i in c(4,6,8)){
+    
+    hydro_names_tbl <- read_sf(x, layer = paste0("WBDHU", i)) |>
+      st_drop_geometry() |>
+      rename(huc = paste0("HUC", i)) |>
+      select(huc, Name) |> 
+      distinct()
+    
+    names(hydro_names_tbl)[2] <- names(hydrologic_units)[hydrologic_units == i]
+    
+    out <- out |> 
+      mutate(huc = substr(hydrologic_unit, 1, i)) |>
+      left_join(hydro_names_tbl, by = "huc")
+    
+  }
+  
+  out |>
+    select(hydrologic_unit, subregion:subbasin, watershed, geometry) |>
+    sf::st_transform(4326)
+  
+}
+
+huc4 <- c(1108, 1301, 1302, 1303, 1305, 1306, 1403,
+          1407, 1408, 1501, 1502, 1504, 1505, 1506)
+
+watersheds <- huc4 |> 
+  map(download_hydro, out_dir = "data-raw") |> 
+  map(read_hydro)
+
+names(watersheds) <- huc4
+
+# colorado river ----------------------------------------------------------
+
 extract_river <- function(x, river){
-  x %>%
-    sf::read_sf(layer = "NHDFlowline") %>%
-    sf::st_zm() %>%
-    dplyr::filter(GNIS_Name == river) %>%
-    sf::st_union() %>%
-    sf::st_cast("MULTILINESTRING") %>%
-    sf::st_line_merge()
+  
+  read_sf(x, layer = "NHDFlowline") |>
+    st_zm() |>
+    filter(GNIS_Name == river) |>
+    st_union() |>
+    st_cast("MULTILINESTRING") |>
+    st_line_merge()
+  
 }
 
 cut_hydro <- function(x, river, side){
-  x %>%
-    sf::st_intersection(x %>%
-                          sf::st_union() %>%
-                          lwgeom::st_split(river) %>%
-                          sf::st_collection_extract("POLYGON") %>%
-                          sf::st_make_valid() %>%
-                          magrittr::extract(side))
+  
+  shp <- st_union(x) |>
+    lwgeom::st_split(river) |>
+    st_collection_extract("POLYGON") |>
+    st_make_valid()
+  
+  st_intersection(x, shp[side])
+  
 }
 
-download_huc <- function(x, 
-                         out_dir = tempdir()){
-  out <- paste0(out_dir,"/NHDPLUS_H_", x, "_HU4_GDB.gdb/")
-  if(file.exists(out))
-    return(out)
-  
-  httr::GET(
-    url = paste0("https://prd-tnm.s3.amazonaws.com/StagedProducts/Hydrography/NHDPlusHR/Beta/GDB/NHDPLUS_H_",x,"_HU4_GDB.zip"),
-    httr::write_disk(tempfile(fileext = ".zip")),
-    httr::progress()
-  ) %$%
-    content %>%
-    magrittr::extract2(1) %>%
-    unzip(exdir = out_dir)
-  
-  return(out)
-}
+cr_lyrs <- list(
+  "data-raw/NHDPLUS_H_1407_HU4_GDB.gdb/",
+  "data-raw/NHDPLUS_H_1501_HU4_GDB.gdb/"
+)
 
-huc4 <- 
-  c(1302, 
-    1303,
-    1403,
-    1407,
-    1408,
-    1501,
-    1502, 
-    1504, 
-    1505, 
-    1506) %>%
-  magrittr::set_names(.,.) %>%
-  purrr::map(download_huc,
-             out_dir = "data-raw") %>%
-  purrr::map(simple_hydro_read)
+colorado_river <- cr_lyrs |> 
+  map(extract_river, river = "Colorado River") |> 
+  map(st_as_sf) |> 
+  bind_rows() |>
+  st_union() |>
+  st_cast("MULTILINESTRING") |>
+  st_line_merge()
 
-colorado_river <-
-  list("data-raw/NHDPLUS_H_1407_HU4_GDB.gdb/",
-       "data-raw/NHDPLUS_H_1501_HU4_GDB.gdb/") %>%
-  purrr::map(extract_river,
-             "Colorado River") %>%
-  purrr::map_dfr(sf::st_as_sf) %>%
-  sf::st_union() %>%
-  sf::st_cast("MULTILINESTRING") %>%
-  sf::st_line_merge()
-
-huc4$`1403` %<>%
-  dplyr::filter(Hydrologic_Unit %in%
-                  c("1403000201",
-                    "1403000202",
-                    "1403000203",
-                    "1403000204",
-                    "1403000206"))
+watersheds$`1403` <- watersheds$`1403` |> 
+  filter(
+    hydrologic_unit %in% c("1403000201", "1403000202", "1403000203", "1403000204", "1403000206")
+  )
 
 # Cut at Colorado River
-huc4$`1407` %<>%
-  dplyr::filter(stringr::str_starts(Hydrologic_Unit, "14070006")) %>%
+watersheds$`1407` <- watersheds$`1407` |> 
+  filter(
+    str_starts(hydrologic_unit, "14070006")
+  ) |>
   cut_hydro(colorado_river, 2)
 
 # Cut at Colorado River, north of Little Colorado
-huc4$`1501` %<>%
-  dplyr::filter(stringr::str_starts(Hydrologic_Unit, "15010001"),
-                Hydrologic_Unit != "1501000106") %>%
+watersheds$`1501` <- watersheds$`1501` |> 
+  filter(
+    str_starts(hydrologic_unit, "15010001"), 
+    hydrologic_unit != "1501000106"
+  ) |>
   cut_hydro(colorado_river, 2)
 
-huc4 %>%
-  dplyr::bind_rows() %>%
-  rmapshaper::ms_simplify(keep = 0.07) %>%
-  sf::write_sf("watersheds.geojson", delete_dsn = TRUE)
+watersheds <- watersheds |>
+  bind_rows() |>
+  rmapshaper::ms_simplify(keep = 0.06) |> 
+  distinct()
+
+write_sf(
+  watersheds, 
+  dsn = "watersheds.geojson", 
+  delete_dsn = TRUE
+)
